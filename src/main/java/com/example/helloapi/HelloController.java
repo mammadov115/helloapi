52d0e674417e6128a581c9c9ffa3bdebe5173446
mammadov115 <mehman.mammadov182@gmail.com>
2026-07-09 11:06:55 +0400

    Add helloPath endpoint to return HelloResponse for path variable

--- src/main/java/com/example/helloapi/HelloController.java
+++ src/main/java/com/example/helloapi/HelloController.java
@@ -3,12 +3,18 @@ package com.example.helloapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public HelloResponse hello(@RequestParam(name = "name", defaultValue = "World") String name) {
+        return new HelloResponse("Hello, " + name + "!");
+    }
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloPath(@PathVariable(name = "name") String name) {
         return new HelloResponse("Hello, " + name + "!");
     }
 
