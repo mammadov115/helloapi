848c0f34455f78407f4589271a29bb4f10f0e6e1
mammadov115 <mehman.mammadov182@gmail.com>
2026-07-09 10:25:21 +0400

    Update hello endpoint to accept a name parameter

--- src/main/java/com/example/helloapi/HelloController.java
+++ src/main/java/com/example/helloapi/HelloController.java
@@ -2,13 +2,14 @@ package com.example.helloapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello world";
+    public String hello(@RequestParam(name = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
     }
 
 }
